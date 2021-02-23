408c1abe8db1c4e7d28974ea83e10a20add6d994
katiezhuang <33261462+katiezhuang@users.noreply.github.com>
2021-02-23 16:50:59 -0500

    added comments

--- RunProject2.java
+++ RunProject2.java
@@ -1,3 +1,9 @@
+/**
+ * This is a driver class to run Project2
+ *
+ * @author Ria Anand
+ * @author Katie Zhuang
+ */
 public class RunProject2 {
     public static void main(String[] args) {
         new PayrollProcessing().run();
