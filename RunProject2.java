c28cfac9c5beceb61b8bacf591e25667ec0f3633
riaanand09 <73204115+riaanand09@users.noreply.github.com>
2021-02-23 19:48:46 -0500

    Update RunProject2.java

--- RunProject2.java
+++ RunProject2.java
@@ -5,8 +5,9 @@
  * @author Katie Zhuang
  */
 public class RunProject2 {
-    public static void main(String[] args) {
-        new PayrollProcessing().run();
-    }
 
+    public static void main(String[] args) {
+        PayrollProcessing process = new PayrollProcessing();
+        process.run();
+    }
 }
